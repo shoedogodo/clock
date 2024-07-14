e733dc4778f7d4b9fa9a784b0dfff05cb3faf031
shoedogodo <46584549+shoedogodo@users.noreply.github.com>
2024-07-14 15:24:52 +0800

    表设计完成

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
+let previousSeconds = new Date().getSeconds();
+
+
+
 function updateClock() {
     const now = new Date();
     const seconds = now.getSeconds();
+    const secondsPlusMilliseconds = now.getSeconds() + now.getMilliseconds() / 1000;
     const minutes = now.getMinutes();
     const hours = now.getHours();
 
-    const secondDegrees = ((seconds / 60) * 360);
+    // //time below is for making clock look prettier
+    // const seconds = 52;
+    // const minutes = 45;
+    // const hours = 9;
+
+    // const secondDegrees = ((seconds / 60) * 360);
+
+
+    const secondDegrees = ((secondsPlusMilliseconds / 60) * 360);
     const minuteDegrees = ((minutes + seconds / 60) / 60) * 360;
     const hourDegrees = ((hours + minutes / 60) / 12) * 360;
 
-    document.getElementById('second-hand').style.transform = `rotate(${secondDegrees}deg)`;
-    document.getElementById('minute-hand').style.transform = `rotate(${minuteDegrees}deg)`;
-    document.getElementById('hour-hand').style.transform = `rotate(${hourDegrees}deg)`;
+    document.getElementById('second-hand-group').style.transform = `rotate(${secondDegrees}deg)`;
+    document.getElementById('minute-hand-group').style.transform = `rotate(${minuteDegrees}deg)`;
+    document.getElementById('hour-hand-group').style.transform = `rotate(${hourDegrees}deg)`;
 
 
     const hoursString = String(hours).padStart(2, '0');
@@ -18,7 +31,10 @@ function updateClock() {
     const secondsString = String(seconds).padStart(2, '0');
     const timeString = `${hoursString}: ${minutesString}: ${secondsString}`;
     document.getElementById('digital-clock').textContent = timeString;
+
+    requestAnimationFrame(updateClock);
 }
 
 setInterval(updateClock, 1000);
 updateClock();
+requestAnimationFrame(updateClock);
